dfe2c5abd4b9093aeb4f545b618efe475303d0be
sbroger <stefan.roger1@gmail.com>
2026-01-17 16:46:56 -0800

    Ensure project compiles out of the box on Windows.

--- include/ProjectCode/DynamicArray.h
+++ include/ProjectCode/DynamicArray.h
@@ -8,5 +8,13 @@ class DynamicArray
 	// See ProjectCode_API.h for more information.
 
 
+	// On Windows, at least one API must be exported in order for a library to be
+	// generated. Without this, UnitTestExe will complain about ProjectCode.lib
+	// not existing, which thwarts building this starter code out of the box.
+	//
+	// Remove this API and comment once you've implemented real content for this class.
+	PROJECTCODE_API void PlaceholderApiFunction() {}
+
+
 	// Assignment part 3: YOUR ANSWER HERE
 };
